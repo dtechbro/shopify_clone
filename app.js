d512fa52c2512f0cfe681c489f3ceca178c51bc6
dtechbro <princeakinola05@gmail.com>
2024-04-02 19:56:00 +0100

    Push all changes

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-list").addEventListener("click", function() {
+    var dropdown = document.getElementById("notification-menu-content");
+    dropdown.classList.toggle("show");
+  });
